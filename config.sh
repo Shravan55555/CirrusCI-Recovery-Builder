d602533c58a723651b839abc629a15d30b18315d
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-07-28 09:18:34 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-12.1
-export DT_LINK=https://github.com/ShazuxD/extras
-export DT_BRANCH=13
-export DEVICE_PATH=device/xiaomi/sea
-export DEVICE=sea
-export MAKEFILE=twrp_sea
-export TARGET=bootimage
-export OUTPUT=boot.img
+export DT_LINK=https://github.com/Shravan55555/recovery_device_oplus_nicky.git
+export DT_BRANCH=ofox_RUI1
+export DEVICE_PATH=device/oplus/nicky
+export DEVICE=nicky
+export MAKEFILE=twrp_nicky
+export TARGET=recoveryimage
+export OUTPUT=recovery.img
 export ALT_FILE=empty
 export RANDOM=17257
