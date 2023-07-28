8ac186e23e403e6fc00b5ac95908c79fb39ee6d8
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-07-28 09:38:23 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-12.1
 export DT_LINK=https://github.com/Shravan55555/recovery_device_oplus_nicky.git
-export DT_BRANCH=ofox_RUI1
+export DT_BRANCH=ofox_13
 export DEVICE_PATH=device/oplus/nicky
 export DEVICE=nicky
 export MAKEFILE=twrp_nicky
